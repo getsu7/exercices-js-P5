734df1f937ce088fe24b2065b842cac13bd3c71a
Nathan COIN <nathan.coin@groupama.com>
2024-06-02 15:50:32 +0200

    exercices-complete

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,14 @@
 //Votre code ici
+function add(a, b) {
+ if (isFloat(a) && isFloat(b)) {
+  return parseInt(a) + parseInt(b);
+ } else {
+  return parseFloat(a) + parseFloat(b)
+ }
+}
+
+function isFloat(n){
+ return Number(n) === n && n % 1 !== 0;
+}
 
  export default add;
